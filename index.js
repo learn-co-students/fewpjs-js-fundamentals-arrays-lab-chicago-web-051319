22f8ba3b639be9e1238f4e70120f074b8ca883fd
John Linatoc <johnlinatoc1227@gmail.com>
2019-06-22 17:35:12 -0500

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push('Odie');
-preprend.unshift('Odie');
+prepend.unshift('Odie');
 removeLast.pop();
 removeFirst.shift();
