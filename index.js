17f48bd7abe204ca4190163d0cd5a8ec2d9aa7a2
github username <github email address>
2019-08-05 11:33:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"] // .push("Odie")
+const prepend = ["Milo", "Otis", "Garfield"] // .unshift("Odie")
+const removeFirst = ["Milo", "Otis", "Garfield"] // .shift
+const removeLast = ["Milo", "Otis", "Garfield"] // .pop
+
+function appendElement(arr , str){
+    return arr.push(str)
+}
+
+function prependElement(arr , str){
+    return arr.unshift(str);
+}
+
+function removeFirstElement(arr){
+    return arr.shift(1)
+}
+
+function removeLastElement(arr){
+    return arr.pop()
+}
+
+let a = "Odie" //forgve this, just for speed
+appendElement(append , a); // not sure if this is how you want me to do this
+prependElement(prepend , a);
+removeFirstElement(removeFirst);
+removeLastElement(removeLast)
+
